ba2c1c2e9fe28d95ad57ab8d2b8e0f47e59c11c3
Mauro Filho <mauro.filho@gmail.com>
2018-04-07 18:50:46 -0300

    feat(Watson): configure sdk bean

---
+++ src/main/java/com/cognitivecare4u/cognitiveapi/images/cognitive/CognitiveService.java
@@ -0,0 +1,10 @@
+package com.cognitivecare4u.cognitiveapi.images.cognitive;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface CognitiveService {
+
+    void listClassifiers();
+
+}
