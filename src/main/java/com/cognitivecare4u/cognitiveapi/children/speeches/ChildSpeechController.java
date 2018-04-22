d007861285e1a570dbcd94dea85304ee540948ed
Mauro Filho <mauro.filho@gmail.com>
2018-04-22 19:01:29 -0300

    add child speech test

--- src/main/java/com/cognitivecare4u/cognitiveapi/children/speeches/ChildSpeechController.java
+++ src/main/java/com/cognitivecare4u/cognitiveapi/children/speeches/ChildSpeechController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
