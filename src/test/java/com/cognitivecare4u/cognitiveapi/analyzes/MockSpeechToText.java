ff8116210a83558c0c38ef4b0b7936e4d71e919d
Mauro Filho <mauro.filho@gmail.com>
2018-05-31 17:40:21 -0300

    test(Mock): add mock to service

---
+++ src/test/java/com/cognitivecare4u/cognitiveapi/analyzes/MockSpeechToText.java
@@ -0,0 +1,17 @@
+package com.cognitivecare4u.cognitiveapi.analyzes;
+
+import com.cognitivecare4u.cognitiveapi.speeches.cognitive.CognitiveSpeechToText;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Primary
+@Component
+public class MockSpeechToText implements CognitiveSpeechToText {
+
+    @Override
+    public String transform(InputStream speech, String contentType) {
+        return "teste de transformação de fala em texto .";
+    }
+}
